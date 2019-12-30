e653e77b6ffca0c9466dab2eeb0be78be38c76c5
Dan Jun <danjun95@gmail.com>
2019-12-29 21:11:24 -0800

    12_29_2019_class

--- degrees-of-separation.py
+++ degrees-of-separation.py
@@ -5,37 +5,38 @@ conf = SparkConf().setMaster("local").setAppName("DegreesOfSeparation")
 sc = SparkContext(conf = conf)
 
 # The characters we wish to find the degree of separation between:
-startCharacterID = 5306 #SpiderMan
-targetCharacterID = 14  #ADAM 3,031 (who?)
+startCharacterID = 5306 #SpiderMan                                              #hard coded start
+targetCharacterID = 14  #ADAM 3,031 (who?)                                      #hard coded end
 
 # Our accumulator, used to signal when we find the target character during
 # our BFS traversal.
 hitCounter = sc.accumulator(0)
 
 def convertToBFS(line):
-    fields = line.split()
-    heroID = int(fields[0])
+    #converts lines in Marvel-Graph.txt into BFS nodes
+    fields = line.split()                                                       #split each item in line into array indexes
+    heroID = int(fields[0])                                                     #takes first ID as the Hero. Rest are associates
     connections = []
     for connection in fields[1:]:
-        connections.append(int(connection))
+        connections.append(int(connection))                                     #inserts hero's associates into a new array
 
     color = 'WHITE'
     distance = 9999
 
-    if (heroID == startCharacterID):
+    if (heroID == startCharacterID):                                            #change the node for the hard-coded starting character
         color = 'GRAY'
         distance = 0
 
-    return (heroID, (connections, distance, color))
+    return (heroID, (connections, distance, color))                             #returns tuple for each line that associates hero to its associates, a color, and a distance
 
 
-def createStartingRdd():
-    inputFile = sc.textFile("file:///sparkcourse/marvel-graph.txt")
+def createStartingRdd():                                                        #creates rdd using Marvel-Graph.txt and turns each line into a desired node
+    inputFile = sc.textFile("C:/Users/Dan/Documents/SparkCourse/Marvel-Graph.txt")
     return inputFile.map(convertToBFS)
 
-def bfsMap(node):
-    characterID = node[0]
-    data = node[1]
+def bfsMap(node):                                                               #used in flatMap() operation to return rdd of size M
+    characterID = node[0]                                                       #<New_Node> : Unknown associates, distance = distance + 1, Color = Gray (yet to process)
+    data = node[1]                                                              #<Old_Node(s)>: color changed to BLACK to show it has been processed
     connections = data[0]
     distance = data[1]
     color = data[2]
@@ -43,12 +44,12 @@ def bfsMap(node):
     results = []
 
     #If this node needs to be expanded...
-    if (color == 'GRAY'):
+    if (color == 'GRAY'):                                                       #only processes the gray nodes
         for connection in connections:
             newCharacterID = connection
             newDistance = distance + 1
             newColor = 'GRAY'
-            if (targetCharacterID == connection):
+            if (targetCharacterID == connection):                               #if the targetCharacterID is part of the associates, the accumulator counter is increased. This can happen in each node in the rdd
                 hitCounter.add(1)
 
             newEntry = (newCharacterID, ([], newDistance, newColor))
@@ -58,19 +59,19 @@ def bfsMap(node):
         color = 'BLACK'
 
     #Emit the input node so we don't lose it.
-    results.append( (characterID, (connections, distance, color)) )
+    results.append( (characterID, (connections, distance, color)) )             #the array of nodes being returned results in new lines in the rdd, thereby increasing the length
     return results
 
-def bfsReduce(data1, data2):
-    edges1 = data1[0]
-    edges2 = data2[0]
-    distance1 = data1[1]
+def bfsReduce(data1, data2):                                                    #used in a .reduceByKey, so no need to consider the actual CharacterIDs
+    edges1 = data1[0]                                                           #takes data from the new Character and original Character
+    edges2 = data2[0]                                                           #the original node is largely going to remain the same
+    distance1 = data1[1]                                                        #the new nodes made of affiliates to the original node(s) will be combined with their "WHITE" colored node to get the associates
     distance2 = data2[1]
     color1 = data1[2]
     color2 = data2[2]
 
     distance = 9999
-    color = color1
+    color = color1                                                              #GRAY
     edges = []
 
     # See if one is the original node with its connections.
@@ -104,25 +105,25 @@ def bfsReduce(data1, data2):
 
 
 #Main program here:
-iterationRdd = createStartingRdd()
+iterationRdd = createStartingRdd()                                              #creates the rdd broken up into nodes
 
-for iteration in range(0, 10):
+for iteration in range(0, 10):                                                  #for loop to check if
     print("Running BFS iteration# " + str(iteration+1))
 
     # Create new vertices as needed to darken or reduce distances in the
     # reduce stage. If we encounter the node we're looking for as a GRAY
     # node, increment our accumulator to signal that we're done.
-    mapped = iterationRdd.flatMap(bfsMap)
+    mapped = iterationRdd.flatMap(bfsMap)                                       #maps each node to the nodes of associates
 
     # Note that mapped.count() action here forces the RDD to be evaluated, and
     # that's the only reason our accumulator is actually updated.
-    print("Processing " + str(mapped.count()) + " values.")
+    print("Processing " + str(mapped.count()) + " values.")                     #this is what makes the previous flatMap(bfsMap) occur. also tells you how much the rdd length increases
 
-    if (hitCounter.value > 0):
-        print("Hit the target character! From " + str(hitCounter.value) \
+    if (hitCounter.value > 0):                                                  #checks if in this iteration, there was a match to the start and end character. The number of iterations needed is the degrees of separation
+        print("Hit the target character! From " + str(hitCounter.value) \       #The "hitCounter" value tells us how many ways it reached that degree of separation
             + " different direction(s).")
         break
 
     # Reducer combines data for each character ID, preserving the darkest
     # color and shortest path.
-    iterationRdd = mapped.reduceByKey(bfsReduce)
+    iterationRdd = mapped.reduceByKey(bfsReduce)                                #recombines the nodes into a single rdd
--- movie-similarities.py
+++ movie-similarities.py
@@ -49,7 +49,7 @@ sc = SparkContext(conf = conf)
 print("\nLoading movie names...")
 nameDict = loadMovieNames()
 
-data = sc.textFile("file:///SparkCourse/ml-100k/u.data")
+data = sc.textFile("C:/Users/Dan/Documents/SparkCourse/ml-100k/u.data")
 
 # Map ratings to key / value pairs: user ID => movie ID, rating
 ratings = data.map(lambda l: l.split()).map(lambda l: (int(l[0]), (int(l[1]), float(l[2]))))
